2185042f0f5e18bcc73e60fa3b5ca51a9ecc0078
anuarbek-zak <anuarbekzakiryanov97@gmail.com>
2018-08-29 02:31:04 +0600

    comt

--- index.controller.js
+++ index.controller.js
@@ -1,11 +1,16 @@
 angular.module('myApp').controller('mainCtrl',function ($http,$scope) {
-    var vm = this;
-    vm.months = ['январь','февраль','март','апрель','май','июнь','июль','август','сентябрь','октябрь','ноябрь','декабрь'];
-    vm.ticketTypes = ['авиабилеты','ж/д билеты','отели'];
-    vm.tabs = ['в одну сторону','в обе стороны','сложный маршрут'];
-    vm.currentTicketType = vm.currentType = 0;
-    $scope.today = function() {
-    $scope.dt = new Date();
-  };
+	var vm = this;
+	vm.months = ['январь','февраль','март','апрель','май','июнь','июль','август','сентябрь','октябрь','ноябрь','декабрь'];
+	vm.ticketTypes = ['авиабилеты','ж/д билеты','отели'];
+	vm.tabs = ['В одну сторону','В обе стороны','Сложный маршрут'];
+	vm.currentTicketType = vm.currentTab = 0;
 
+
+	$http.get("https://santufei.com/api/v1/content/directions/?city_a=ALA&city_b=IS")
+	.then(function(response) {
+		console.log('resp',response)
+	},function(err){
+				console.log('err',err)
+
+	});
 });
