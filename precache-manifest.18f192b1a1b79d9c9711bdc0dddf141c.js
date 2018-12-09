4dd2a9e3c79f5091786f4bd830bd249374a08b92
anuarbek-zak <anuarbekzakiryanov97@gmail.com>
2018-12-09 19:10:58 +0600

    done

---
+++ precache-manifest.18f192b1a1b79d9c9711bdc0dddf141c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa62c04b03d3f7d2333a33c311d9747d",
+    "url": "/static/media/Lato-Regular.fa62c04b.ttf"
+  },
+  {
+    "revision": "ca811d509892cc9826fc",
+    "url": "/static/css/main.a7a99a3e.chunk.css"
+  },
+  {
+    "revision": "5c3d84d6678111c24ff2",
+    "url": "/static/js/1.5c3d84d6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b206e1b6c717af04a5ab333d1ab7ef00",
+    "url": "/static/media/logo.b206e1b6.svg"
+  },
+  {
+    "revision": "4b9cea425a2de217c2468ac8eff142bf",
+    "url": "/static/media/logo.4b9cea42.svg"
+  },
+  {
+    "revision": "7a26fe5412f9200c8b93ec251d4dd464",
+    "url": "/static/media/cart.7a26fe54.svg"
+  },
+  {
+    "revision": "9ec7d5a0bff7258d9e4ecb744ad54337",
+    "url": "/static/media/avatar.9ec7d5a0.svg"
+  },
+  {
+    "revision": "9d136de7ef5eb26e9dc817a209ee3056",
+    "url": "/static/media/content-text.9d136de7.svg"
+  },
+  {
+    "revision": "a21e6f77e061d973998cf366ec1c7d39",
+    "url": "/static/media/slider.a21e6f77.png"
+  },
+  {
+    "revision": "ca811d509892cc9826fc",
+    "url": "/static/js/main.ca811d50.chunk.js"
+  },
+  {
+    "revision": "d496e08e5651281b29500dad0ea7b930",
+    "url": "/static/media/search.d496e08e.svg"
+  },
+  {
+    "revision": "01dcf602cb7f7116bde159f25c4bbe89",
+    "url": "/static/media/Lato-Bold.01dcf602.ttf"
+  },
+  {
+    "revision": "47df357749e2185a9b8a344c722deba0",
+    "url": "/static/media/Lato-Semibold.47df3577.ttf"
+  },
+  {
+    "revision": "b9aedd8ab0da366678c54d64b82edd88",
+    "url": "/static/media/cart-white.b9aedd8a.svg"
+  },
+  {
+    "revision": "fc4c22a858df0ed012c203d828a82663",
+    "url": "/static/media/basket.fc4c22a8.svg"
+  },
+  {
+    "revision": "5acc644c2cb5c3d945f5c6a8eed35221",
+    "url": "/static/media/basket-wite.5acc644c.svg"
+  },
+  {
+    "revision": "f05afee33a6f1c86da1681272c14e6cb",
+    "url": "/static/media/vegetables.f05afee3.svg"
+  },
+  {
+    "revision": "9ba5500e35cb9a07eb8aebbc720d4192",
+    "url": "/static/media/banan.9ba5500e.png"
+  },
+  {
+    "revision": "a959e0acb0b6abe9d50c5feb13b72775",
+    "url": "/static/media/main-bg.a959e0ac.jpg"
+  },
+  {
+    "revision": "99f53448c2acedc2b3f6effa227b9993",
+    "url": "/index.html"
+  }
+];
